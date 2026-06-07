5d0aa1dbc299f647a536279da6f09b72b5a563f4
paulocomp <paulocorreacomp@gmail.com>
2026-06-07 03:00:19 -0300

    Adiciona método para remover (tornar null) categoria de produtos

--- src/main/java/com/bizuinfo/produto/dao/ProdutoDAO.java
+++ src/main/java/com/bizuinfo/produto/dao/ProdutoDAO.java
@@ -3,7 +3,6 @@ package com.bizuinfo.produto.dao;
 import com.bizuinfo.infra.dao.GenericoDAO;
 import com.bizuinfo.infra.util.JPAutil;
 import com.bizuinfo.produto.model.Produto;
-
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.NoResultException;
@@ -21,18 +20,50 @@ public class ProdutoDAO extends GenericoDAO<Produto> {
 
         try (EntityManager em = JPAutil.getEntityManager()) {
 
-            Produto p = em.createQuery(
+            Produto produto = em.createQuery(
                             "SELECT p FROM Produto p WHERE p.nome = :nome",
                             Produto.class
                     )
                     .setParameter("nome", nome)
                     .getSingleResult();
 
-            return Optional.of(p);
+            return Optional.of(produto);
 
         } catch (NoResultException e) {
 
             return Optional.empty();
+        }
+    }
+
+    public void removerCategoriaDosProdutos(Long categoriaId) {
+
+        EntityManager em = JPAutil.getEntityManager();
+
+        try {
+
+            em.getTransaction().begin();
+
+            em.createQuery("""
+                UPDATE Produto p
+                SET p.categoria = NULL
+                WHERE p.categoria = :categoriaId
+                """)
+                    .setParameter("categoriaId", categoriaId)
+                    .executeUpdate();
+
+            em.getTransaction().commit();
+
+        } catch (Exception e) {
+
+            if (em.getTransaction().isActive()) {
+                em.getTransaction().rollback();
+            }
+
+            throw e;
+
+        } finally {
+
+            em.close();
         }
     }
 }
