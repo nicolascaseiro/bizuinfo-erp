e45a77969a91efb0d287293f8a173bdf2808f35d
paulocomp <paulocorreacomp@gmail.com>
2026-06-07 03:02:44 -0300

    Adiciona Serviço EJB ponte entre Bean de Categorias e DAO de Categorias

---
+++ src/main/java/com/bizuinfo/produto/service/CategoriaService.java
@@ -0,0 +1,45 @@
+package com.bizuinfo.produto.service;
+
+import com.bizuinfo.produto.dao.CategoriaDAO;
+import com.bizuinfo.produto.dao.ProdutoDAO;
+import com.bizuinfo.produto.model.Categoria;
+import jakarta.ejb.Stateless;
+import jakarta.inject.Inject;
+
+@Stateless
+public class CategoriaService {
+    @Inject
+    private CategoriaDAO categoriaDAO;
+
+    @Inject
+    private ProdutoDAO produtoDAO;
+
+    public void atualizarNome(Long categoriaId, String novoNome) {
+
+        Categoria categoria = categoriaDAO
+                .buscarPorId(categoriaId)
+                .orElseThrow();
+
+        categoria.setNome(novoNome);
+
+        categoriaDAO.salvar(categoria);
+    }
+
+    public void excluirCategoria(Long categoriaId) {
+
+        produtoDAO.removerCategoriaDosProdutos(categoriaId);
+
+        categoriaDAO.remover(categoriaId);
+    }
+
+    public void criarCategoria(String nome) {
+
+        if (nome == null || nome.isBlank()) {
+            throw new RuntimeException("O nome da categoria é obrigatório.");
+        }
+
+        Categoria categoria = new Categoria();
+        categoria.setNome(nome.trim());
+        categoriaDAO.salvar(categoria);
+    }
+}
